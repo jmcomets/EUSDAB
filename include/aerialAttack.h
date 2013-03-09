c21840ae291b1feafd822fdb2f2a0269f5b6d7c0
Ayuzawa <scarensac@scarensac-VirtualBox.(none)>
2013-03-09 11:59:04 +0100

    etats avancés, gitignore modifié pour les fichiers backup

---
+++ include/aerialAttack.h
@@ -0,0 +1,14 @@
+#ifndef AERIALATTACK_H_
+#define AERIALATTACK_H_
+
+#include <characterState.h>
+
+class AerialAttack: public CharacterState
+{
+	public:
+	
+	protected:
+
+};
+
+#endif /* AERIALATTACK_H_ */
---
+++ include/ejected.h
@@ -0,0 +1,14 @@
+#ifndef EJECTED_H_
+#define EJECTED_H_
+
+#include <characterState.h>
+
+class Ejected: public CharacterState
+{
+	public:
+	
+	protected:
+
+};
+
+#endif /* EJECTED_H_ */
