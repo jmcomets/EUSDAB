bd1f80316608ddcf3bb3dbdbd41e5f51e0b64c0b
Ayuzawa <scarensac@scarensac-VirtualBox.(none)>
2013-03-09 12:13:26 +0100

    ceci est un message de commit

---
+++ include/aerialAttackUp.h
@@ -0,0 +1,14 @@
+#ifndef AERIALATTACKRIGHT_H_
+#define AERIALATTACKRIGHT_H_
+
+#include <aerialAttack.h>
+
+class AerialAttackRight: public AerialAttack
+{
+	public:
+	
+	protected:
+	
+};
+
+#endif /* AERIALATTACKUP_H_ */
