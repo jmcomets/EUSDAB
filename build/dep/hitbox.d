2ba301bec2bb6c2703d0b069601ee261b8a67768
马力 <alex.peyrard@gmail.com>
2013-03-09 22:01:31 +0100

    t

--- build/dep/hitbox.d
+++
@@ -1,2 +0,0 @@
-build/obj/hitbox.o: src/hitbox.cpp include/hitbox.h include/polygone.h \
- include/point.h
--- build/dep/joystick.d
+++
@@ -1,4 +0,0 @@
-build/obj/joystick.o: src/joystick.cpp include/joystick.h \
- lib/SFML-2.0-rc/include/SFML/Window/Joystick.hpp \
- lib/SFML-2.0-rc/include/SFML/Window/Export.hpp \
- lib/SFML-2.0-rc/include/SFML/Config.hpp
