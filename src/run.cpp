6d0d6ddc60cc9b5da3e20d0109d8fc48575b807c
Turpin Pierre <pierre.turpin@insa-lyon.fr>
2013-03-09 21:15:01 +0100

    Allez la

--- src/run.cpp
+++ src/run.cpp
@@ -3,81 +3,81 @@
 #include <character.h>
 #include <joystickState.h>
 
-Run(Character &c, float speed):
-	CharacterState(c)
+Run::Run(Character & c, float speed, DirectionX dirX, DirectionY dirY) :
+    CharacterState(c, dirX, dirY)
 {
-	if(isDirection(Right))
-	{
-		_motion.x = speed;
-		_motion.y = 0.f;
-	}
-	else
-	{
-		_motion.x = -speed;
-		_motion.y = 0.f;
-	}
+    if(isDirection(Right))
+    {
+        _motion.x = speed;
+        _motion.y = 0.f;
+    }
+    else
+    {
+        _motion.x = -speed;
+        _motion.y = 0.f;
+    }
 }
 
-~Run()
+Run::~Run()
 {
 }
 
 void Run::enter()
 {
-	CharacterState::enter();
+    CharacterState::enter();
 }
 
-void Run::update(const JoystickState j)
+void Run::update(const JoystickState & j)
 {
-	CharacterState::update();
-	if (j.axisPosition(_character.joystickId(), JoystickState::Axis::X) * _motion.x = 0)
-	{
-		if (isDirection(Right))
-		{
-			_character.state(CharacterState::IdleRight);
-		}
-		else
-		{
-			_character.state(CharacterState::IdleLeft);
-		}
-	}
-	else if (j.axisPosition(_character.joystickId(), JoystickState::Axis::X) * _motion.x < 0)
-	{
-		if(isDirection(Left))
-		{
-			_character.state(CharacterState::RunRight);
-		}
-		else
-		{
-			_character.state(CharacterState::RunLeft);
-		}
-	}
-	else
-	{
-		_character.move(_motion);
-		/*if (j.isButtonFront(_character.joystickId(),BUTTON_A)) 
-		{
-			if (isDirection(Left))
-			{
-				_character.state(CharacterState::AttackLeft);
-			}
-			else
-			{
-				_character.state(CharacterState::AttackRight);
-			}
-		}
-		else if (j.isButtonFront(_character.joystickId(), BUTTON_B))
-		{
-			if (isDirection(Left))
-			{
-				_character.state(CharacterState::SpecialLeft);
-			}
-			else
-			{
-				_character.state(CharacterState::SpecialRight);
-			}
-		}*/
-	}
+    CharacterState::update(j);
+    if (j.axisPosition(_character.joystickId(), JoystickState::Axis::X) * _motion.x == 0)
+    {
+        if (isDirection(Right))
+        {
+            _character.state(CharacterState::IdleRight);
+        }
+        else
+        {
+            _character.state(CharacterState::IdleLeft);
+        }
+    }
+    else if (j.axisPosition(_character.joystickId(), JoystickState::Axis::X) * _motion.x < 0)
+    {
+        if(isDirection(Left))
+        {
+            _character.state(CharacterState::RunRight);
+        }
+        else
+        {
+            _character.state(CharacterState::RunLeft);
+        }
+    }
+    else
+    {
+        _character.move(_motion);
+        /*if (j.isButtonFront(_character.joystickId(),BUTTON_A)) 
+        {
+            if (isDirection(Left))
+            {
+                _character.state(CharacterState::AttackLeft);
+            }
+            else
+            {
+                _character.state(CharacterState::AttackRight);
+            }
+        }
+        else if (j.isButtonFront(_character.joystickId(), BUTTON_B))
+        {
+            if (isDirection(Left))
+            {
+                _character.state(CharacterState::SpecialLeft);
+            }
+            else
+            {
+                _character.state(CharacterState::SpecialRight);
+            }
+        }*/
+    }
 }
 
 void Run::leave()
