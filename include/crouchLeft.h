dfa5947831346c754def4fe2df6da3117c35eb21
马力 <alex.peyrard@gmail.com>
2013-03-09 12:40:03 +0100

    coucou

---
+++ include/crouchLeft.h
@@ -0,0 +1,11 @@
+//-------------crouchLeft.h----------------------//
+
+#ifndef CROUCH_LEFT_H_INCLUDED_
+#define CROUCH_LEFT_H_INCLUDED_
+
+class CrouchLeft : public Crouch
+{
+
+}
+
+#endif
---
+++ include/crouchRight.h
@@ -0,0 +1,11 @@
+//-------------crouchRight.h----------------------//
+
+#ifndef CROUCH_RIGHT_H_INCLUDED_
+#define CROUCH_RIGHT_H_INCLUDED_
+
+class CrouchRight : public Crouch
+{
+
+}
+
+#endif
