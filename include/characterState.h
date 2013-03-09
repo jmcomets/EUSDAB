5c70619d20664fea1b07d4e8f9bb8fec238612c3
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-03-09 17:53:20 +0100

    Ajout du système complèxe pour le joystick

--- include/characterState.h
+++ include/characterState.h
@@ -3,6 +3,7 @@
 
 #include <view.h>
 #include <hitbox.h>
+#include <joystickState.h>
 
 class Character;
 
@@ -41,7 +42,7 @@ class CharacterState
         virtual ~CharacterState();
         CharacterState & operator=(const CharacterState &);
 
-        virtual void update();
+        virtual void update(const JoystickState &);
         virtual void enter();
         virtual void leave();
 
