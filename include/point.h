e42ccc368af821e5d6268e5c4e2079675f3bda11
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-03-09 22:57:19 +0100

    Point class done

--- include/point.h
+++ include/point.h
@@ -1,26 +1,50 @@
-#ifndef __POINT_H__
-#define __POINT_H__
+#ifndef POINT_H_
+#define POINT_H_
 
 namespace Geometry
 {
     template <typename T>
-    class Point
+        class Point
     {
         public:
-            explicit Point(T const & x, T const & y);
-            virtual ~Point();
+            Point():
+                Point(0, 0)
+            {
+            }
 
-            T const & x() const;
-            T const & y() const;
+            Point(const T & x, const T & y):
+                _x(x), _y(y)
+            {
+            }
+            
+            Point(Point &&) = default;
+            Point(const Point &) = default;
+            ~Point() = default;
+            Point & operator=(const Point &) = default;
 
-            void setX(T const &);
-            void setY(T const &);
+            const T & x() const
+            {
+                return _x;
+            }
 
-        protected:
-            T _x;
-            T _y;
+            const T & x(const T & x)
+            {
+                return _x = x;
+            }
+
+            const T & y() const
+            {
+                return _y;
+            }
+
+            const T & y(const T &)
+            {
+                return _y = y;
+            }
+
+        private:
+            T _x, _y;
     };
 }
 
 #endif
-
