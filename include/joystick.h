c4b25331d76d4470813a6dfaded38d530bcda40d
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-03-09 19:26:01 +0100

    Added new joystick interface

---
+++ include/joystick.h
@@ -0,0 +1,71 @@
+#ifndef JOYSTICK_H_
+#define JOYSTICK_H_
+
+namespace Joystick
+{
+    enum Axis
+    {
+        X, Y
+    };
+
+    enum Button
+    {
+        ButtonA, ButtonB,
+        ButtonZ, ButtonY, //ButtonX,
+        TriggerLeft, TriggerRight,
+        Other
+    };
+
+    class State
+    {
+        public:
+            template <typename Iterator>
+                State(unsigned int id, Iterator begin, Iterator end):
+                    _id(id), _btnConfig(begin, end)
+            {
+                _btnsUp[ButtonA] = false;
+                _btnsUp[ButtonB] = false;
+                _btnsUp[ButtonZ] = false;
+                _btnsUp[ButtonY] = false;
+                _btnsUp[TriggerLeft] = false;
+                _btnsUp[TriggerRight] = false;
+                _btnsUp[Other] = false;
+            }
+
+            State(unsigned int);
+            State(State &&);
+            State(const State &);
+            ~State();
+            State & operator=(const State &);
+            bool isConnected() const;
+            bool isAxisFront(Axis) const;
+            float axisPosition(Axis) const;
+            bool isButtonFront(Button) const;
+            bool isButtonUp(Button) const;
+            bool isButtonDown(Button) const;
+
+        private:
+            std::vector<Button> _btnConfig;
+            unsigned int _id;
+            std::unordered_map<Button, bool> _btnsUp;
+            std::pair<bool, bool> _axesUp;
+    };
+
+    class System
+    {
+        public:
+            System();
+            System(System &&);
+            System(const System &);
+            ~System();
+            System & operator=(const System &);
+            void init();
+            void update();
+            const State & state(unsigned int) const;
+
+        private:
+            std::unordered_map<unsigned int, State> _states;
+    };
+}
+
+#endif
