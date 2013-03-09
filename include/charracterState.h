b25e2163d6351d62e0ea5fbb123dfca05aaae6a3
马力 <alex.peyrard@gmail.com>
2013-03-09 11:10:15 +0100

    ajout de characterState

---
+++ include/charracterState.h
@@ -0,0 +1,12 @@
+//-----------file characterState.h----------//
+//ceci est l'etat général dont les autres hériteront
+
+#ifndef CHARACTER_STATE_H_INCLUDED_
+#define CHARACTER_STATE_H_INCLUDED_
+
+//includes
+
+class CharacterState
+{
+
+}
