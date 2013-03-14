aaa8d010eca69b1208cbe6827e953dd20081901b
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-03-14 13:20:20 +0100

    entity state system added

--- include/entity.h
+++ include/entity.h
@@ -1,9 +1,11 @@
 #ifndef ENTITY_H_
 #define ENTITY_H_
 
+#include <unordered_map>
 #include <SFML/System/Vector2.hpp>
 #include <graphics.h>
 #include <view.h>
+#include <state.h>
 
 class Entity
 {
@@ -11,16 +13,22 @@ class Entity
         Entity() = default;
         Entity(const sf::Vector2f &);
         Entity(Entity &&) = default;
-        Entity(const Entity &) = default;
+        Entity(const Entity &) = delete;
         virtual ~Entity();
-        Entity & operator=(const Entity &) = default;
+        Entity & operator=(const Entity &) = delete;
         const sf::Vector2f & position() const;
         const sf::Vector2f & position(const sf::Vector2f &);
         const sf::Vector2f & move(const sf::Vector2f &);
         virtual void update() = 0;
         virtual void render(Graphics::Target &, Graphics::RenderStates) = 0;
+        void addState(State::Id, State *);
+        State * state() const;
+        State * state(State::Id) const;
+        void setState(State *);
 
     protected:
+        State * _current;
+        std::unordered_map<State::Id, State *, std::hash<int>> _states;
         sf::Vector2f _pos;
 };
 
--- src/entity.cpp
+++ src/entity.cpp
@@ -1,4 +1,5 @@
 #include <entity.h>
+#include <stdexcept>
 
 Entity::Entity(const sf::Vector2f & p):
     _pos(p)
@@ -7,6 +8,10 @@ Entity::Entity(const sf::Vector2f & p):
 
 Entity::~Entity()
 {
+    for (auto p : _states)
+    {
+        delete p->second;
+    }
 }
 
 const sf::Vector2f & Entity::position() const 
@@ -22,4 +27,28 @@ const sf::Vector2f & Entity::position(const sf::Vector2f & p)
 const sf::Vector2f & Entity::move(const sf::Vector2f & p)
 {
     return _pos += p;
+}
+
+void Entity::addState(State::Id id, State * state)
+{
+    if (_states.emplace(id, state).second == false)
+    {
+        throw std::runtime_error("Entity's States should be unique");
+    }
+}
+
+State * Entity::state() const
+{
+    return _current;
+}
+
+State * Entity::state(State::Id id) const
+{
+    auto it = _states.find(id);
+    return (it != _states.end()) ?  nullptr : it->second;
+}
+
+void Entity::setState(State * state)
+{
+    _current = state;
 }
