a969c6b16b91965f498032a83143405a0ea3d69a
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-03-26 15:29:27 +0100

    Entity with mask...

---
+++ src/entitywithmask.cpp
@@ -0,0 +1,28 @@
+#include <entitywithmask.h>
+
+namespace EUSDAB
+{
+    EntityWithMask::EntityWithMask(Input::State * current):
+        Entity(current), _mask()
+    {
+    }
+
+    EntityWithMask::~EntityWithMask()
+    {
+    }
+
+    void EntityWithMask::mask(const Entity * e)
+    {
+        _mask.insert(e);
+    }
+
+    bool EntityWithMask::masked(const Entity * e) const
+    {
+        return _mask.find(e) != _mask.end();
+    }
+
+    void EntityWithMask::reset()
+    {
+        _mask.clear();
+    }
+}
