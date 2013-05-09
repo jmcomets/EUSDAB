a384628442505f82dc489cb66886df6774f68b3c
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-05-09 15:50:42 +0200

    flipped rick_hard animations

--- scripts/FlipAnimation.py
+++ scripts/FlipAnimation.py
@@ -11,24 +11,21 @@ out_folder_suffix = 'out'
 
 def main(in_folder):
     out_folder = os.path.join(in_folder, out_folder_suffix)
-    try:
-        in_json = os.path.join(in_folder, json_file_basename)
-        if not os.path.exists(out_folder):
-            os.mkdir(out_folder)
-        out_json = os.path.join(out_folder, json_file_basename)
-        dct = json.load(open(in_json, 'r'))
-        frames = dct['frames']
-        for filename, hitbox_list in frames.items():
-            for hb in hitbox_list:
-                hb['center']['x'] *= -1.
-            in_filename = os.path.join(in_folder, filename)
-            out_filename = os.path.join(out_folder, filename)
-            img = Image.open(in_filename)
-            img = img.transpose(Image.FLIP_LEFT_RIGHT)
-            img.save(out_filename)
-        json.dump(dct, open(out_json, 'w'))
-    except IOError, OSError:
-        print e
+    in_json = os.path.join(in_folder, json_file_basename)
+    if not os.path.exists(out_folder):
+        os.mkdir(out_folder)
+    out_json = os.path.join(out_folder, json_file_basename)
+    dct = json.load(open(in_json, 'r'))
+    frames = dct['frames']
+    for filename, hitbox_list in frames.items():
+        for hb in hitbox_list:
+            hb['center']['x'] *= -1.
+        in_filename = os.path.join(in_folder, filename)
+        out_filename = os.path.join(out_folder, filename)
+        img = Image.open(in_filename)
+        img = img.transpose(Image.FLIP_LEFT_RIGHT)
+        img.save(out_filename)
+    json.dump(dct, open(out_json, 'w'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
