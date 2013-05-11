04c1bf95aa6c2e4cfefe6e8edfe013fef30824aa
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2013-05-11 17:15:25 +0200

    TextureManger::loadTexture() bug corrected

--- src/texturemanager.cpp
+++ src/texturemanager.cpp
@@ -1,5 +1,6 @@
 #include <texturemanager.h>
 #include <stdexcept>
+#include <iostream>
 
 namespace EUSDAB
 {
@@ -26,10 +27,33 @@ namespace EUSDAB
                 const std::string & filename)
         {
             TextureManager * inst = instance();
+            auto it = inst->_textures.find(filename);
+            if(it == inst->_textures.end()) // New texture
+            {
+                sf::Texture * tx = new sf::Texture();
+                if(tx->loadFromFile(filename))
+                {
+                    TexturePtr ptr(tx);
+                    inst->_textures.insert(std::make_pair(filename, ptr));
+                    return ptr;
+                }
+                else
+                {
+                    throw std::runtime_error("Texture couldn't be loaded");
+                }
+            }
+            else
+            {
+                return it->second;
+            }
+
+            /*
+            TextureManager * inst = instance();
             std::pair<std::string, TexturePtr> empty_pair(filename, nullptr);
             auto insert_pair = inst->_textures.insert(empty_pair);
             if (insert_pair.second) // inserted -> new (not yet loaded)
             {
+                std::cout << "IN TEXTURE MANAGER" << std::endl;
                 auto it = insert_pair.first;
                 TexturePtr ptr(it->second);
                 sf::Texture * tx = new sf::Texture();
@@ -46,8 +70,10 @@ namespace EUSDAB
             }
             else
             {
+                std::cout << "THIS IS BULLSHIT !" << std::endl;
                 return insert_pair.first->second;
             }
+            //*/
         }
     }
 }
--- tests/joystick/testJoystick.cpp
+++ tests/joystick/testJoystick.cpp
@@ -22,7 +22,7 @@ namespace EUSDAB
         // Creating players
         Entity * player1 = entityParser.loadEntity("../../assets/entities/rickhard");
         //Entity * player1 = new Entity();
-        //Entity * player2 = entityParser.loadEntity("../../assets/entities/rickhard2");
+        Entity * player2 = entityParser.loadEntity("../../assets/entities/rickhard");
         //Entity * player2 = new Entity();
         
         if (player1 == nullptr)
@@ -34,12 +34,12 @@ namespace EUSDAB
 
         //player2->state()->animation.sprite()->setPosition(300, 250);
         _painter.addEntity(player1);
-        //_painter.addEntity(player2);
+        _painter.addEntity(player2);
 
 
         // Adding players
         _playerList.emplace_back(player1);
-        //_playerList.emplace_back(player2);
+        _playerList.emplace_back(player2);
 
         //_mapping = new Input::KeyboardMapping(_playerList.begin(), _playerList.end());
         _mapping = new Input::JoystickMapping(_playerList.begin(), _playerList.end());
