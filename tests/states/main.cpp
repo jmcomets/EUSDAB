c77c205d18c5a093e2c79edc2db8e156f7e31f40
Turpin <pierre.turpin@insa-lyon.fr>
2013-05-15 00:53:51 +0200

    removing dummy content

--- tests/states/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "teststates.h"
-
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 500), "EUSDAB");
-    EUSDAB::Application * app = new EUSDAB::StatesTest(window);
-    app->run();
-    return 0;
-}
