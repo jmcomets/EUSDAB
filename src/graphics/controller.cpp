f72b51da21c979f1d80acf4673dbac4f8f0feccc
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-05-15 21:55:10 +0200

    Added camera system

--- src/graphics/controller.cpp
+++ src/graphics/controller.cpp
@@ -14,6 +14,9 @@ namespace EUSDAB
 
         void Controller::draw()
         {
+            // Barycenter of the entities
+            Physics::Vector2 barycenter;
+
             for (auto e : _entities)
             {
                 // Check that entity's state is non-nil
@@ -29,7 +32,17 @@ namespace EUSDAB
                 const Physics::Vector2 & p = e->position();
                 sp.setPosition(p.x(), p.y());
                 _target.draw(sp);
+
+                // Add position vector to barycenter
+                barycenter += p;
             }
+
+            // Convert to barycenter by dividing by number of entities
+            barycenter /= _entities.size();
+            sf::Vector2f sfBarycenter(barycenter.x(), barycenter.y());
+            sf::View view = _target.getView();
+            view.move(sfBarycenter - view.getCenter());
+            _target.setView(view);
         }
 
         void Controller::addEntity(Entity * entity)
