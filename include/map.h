e5f89c3feb2710767bed44a6d0cee0ae6ede85cf
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-05-20 14:47:16 +0200

    Life retabbed

--- include/map.h
+++ include/map.h
@@ -1,32 +1,31 @@
 #ifndef MAP_H_
 #define MAP_H_
 
-#include <string>
 #include <list>
-#include <utility>
 #include <entity.h>
 #include <graphics/texturemanager.h>
-#include <physics/config.h>
 #include <SFML/Graphics/Sprite.hpp>
 
 namespace EUSDAB
 {
-	class Map: public Entity
-	{
-		public:
-			Map(Map &&) = default;
-			Map(Map const &) = delete;
-			Map & operator=(Map const &) = delete;
+    class Map: public Entity
+    {
+        public:
+            Map(Map &&) = default;
+            Map(Map const &) = delete;
+            Map & operator=(Map const &) = delete;
 
-			Map();
-			virtual ~Map();
-			
-			//animated background
-			void addAnimatedBackground(Graphics::TextureManager::TexturePtr,Physics::Vector2);
+            Map();
+            virtual ~Map();
 
-		private:
-			std::list<std::pair<sf::Sprite,Physics::Vector2>> _animatedBackgroundList;
-	};
+            typedef Graphics::TextureManager::TexturePtr TexturePtr;
+
+            // Add animated background to the map
+            void addAnimatedBackground(TexturePtr, const Physics::Vector2 &);
+
+        private:
+            std::list<std::pair<sf::Sprite, Physics::Vector2>> _animatedBackgroundList;
+    };
 }
 
 #endif
--- src/map.cpp
+++ src/map.cpp
@@ -6,26 +6,22 @@
 
 namespace EUSDAB
 {
-	Map::Map():
-		Entity()
-	{
-		_gravitable =false;
-		_life = new InfiniteLife;
-		_nbrJumpMax = 0;
-		_nbrJumpLeft = 0;
-		_jumpPossible = false;
-	}
+    Map::Map():
+        Entity()
+    {
+        _gravitable =false;
+        _life = new InfiniteLife();
+        _nbrJumpMax = 0;
+        _nbrJumpLeft = 0;
+        _jumpPossible = false;
+    }
 
-	Map::~Map()
-	{
-		for (auto s : _states)
-		{
-			delete s;
-		}
-	}
+    Map::~Map()
+    {
+    }
 
-	void Map::addAnimatedBackground(Graphics::TextureManager::TexturePtr t,Physics::Vector2 v)
-	{
-		_animatedBackgroundList.push_back(std::make_pair(sf::Sprite(*t),v));
-	}
+    void Map::addAnimatedBackground(Graphics::TextureManager::TexturePtr t,Physics::Vector2 v)
+    {
+        _animatedBackgroundList.push_back(std::make_pair(sf::Sprite(*t),v));
+    }
 }
