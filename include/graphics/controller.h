ce6e1114ca8e6425ce70e323af3b8743b4935041
Turpin <pierre.turpin@insa-lyon.fr>
2013-05-21 03:29:15 +0200

    HUD

--- include/graphics/controller.h
+++ include/graphics/controller.h
@@ -1,9 +1,10 @@
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
 
+#include <array>
 #include <cassert>
 #include <set>
-#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics.hpp>
 #include <entity.h>
 #include <physics/world.h>
 
@@ -27,9 +28,34 @@ namespace EUSDAB
                             const Physics::World * world):
                         _target(target), _entityList(),
                             _playerList(begin, end),
-                            _world(world)
+                            _world(world),
+                            _lsChar(),
+                            _texRickHard(),
+                            _texCharlie(),
+                            _texPedroPanda(),
+                            _shader_rainbow(),
+                            _shader_filter()
                 {
                     assert(_world != nullptr);
+
+                    _lsChar[0].loadFromFile("../../assets/hud/number_0.png");
+                    _lsChar[1].loadFromFile("../../assets/hud/number_1.png");
+                    _lsChar[2].loadFromFile("../../assets/hud/number_2.png");
+                    _lsChar[3].loadFromFile("../../assets/hud/number_3.png");
+                    _lsChar[4].loadFromFile("../../assets/hud/number_4.png");
+                    _lsChar[5].loadFromFile("../../assets/hud/number_5.png");
+                    _lsChar[6].loadFromFile("../../assets/hud/number_6.png");
+                    _lsChar[7].loadFromFile("../../assets/hud/number_7.png");
+                    _lsChar[8].loadFromFile("../../assets/hud/number_8.png");
+                    _lsChar[9].loadFromFile("../../assets/hud/number_9.png");
+                    _lsChar[10].loadFromFile("../../assets/hud/percent.png");
+
+                    _texRickHard.loadFromFile("../../assets/hud/jauge_rickhard.png");
+                    _texCharlie.loadFromFile("../../assets/hud/jauge_charlie.png");
+                    _texPedroPanda.loadFromFile("../../assets/hud/jauge_pedropanda.png");
+
+                    _shader_rainbow.loadFromFile("../../assets/shader/wave.vert", sf::Shader::Vertex);
+                    _shader_filter.loadFromFile("../../assets/shader/filter.vert", sf::Shader::Vertex);
                 }
 
                 // Draw the Controller to its currently set target
@@ -41,12 +67,12 @@ namespace EUSDAB
                 // ...range version
                 template <typename InputIter>
                     void addEntity(InputIter begin, InputIter end)
-                {
-                    for (; begin != end ; begin++)
                     {
-                        addEntity(*begin);
+                        for (; begin != end ; begin++)
+                        {
+                            addEntity(*begin);
+                        }
                     }
-                }
 
                 // Remove an Entity from the Controller
                 void removeEntity(Entity * entity);
@@ -74,6 +100,13 @@ namespace EUSDAB
 
                 // Physics world, used by camera
                 const Physics::World * _world;
+
+                std::array<sf::Texture, 11> _lsChar;
+                sf::Texture _texRickHard;
+                sf::Texture _texCharlie;
+                sf::Texture _texPedroPanda;
+                sf::Shader _shader_rainbow;
+                sf::Shader _shader_filter;
         };
     }
 }
