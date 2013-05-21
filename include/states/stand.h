901bd45b88e5681561f362d1d7c8f8bc3eae7241
Ayuzawa <scarensac@scarensac-VirtualBox.(none)>
2013-05-21 17:42:37 +0200

    ajout des state stunned et stand

---
+++ include/states/stand.h
@@ -0,0 +1,49 @@
+#ifndef STATES_STAND_H
+#define STATES_STAND_H
+
+#include <state.h>
+
+namespace EUSDAB
+{
+    namespace States
+    {
+        class Stand: public State
+        {
+            public:
+                Stand(Stand &&) = default;
+                Stand(const Stand &) = delete;
+                Stand & operator=(const Stand &) = delete;
+
+                Stand(Movement const &);
+                virtual ~Stand();
+
+                void onUp(const Event &);
+                void onDown(const Event &);
+                void onLeft(const Event &);
+                void onRight(const Event &);
+
+                void onA(const Event &);
+                void onB(const Event &);
+                //void onX(const Event &);
+                //void onY(const Event &);
+                //void onZ(const Event &);
+                //void onTrigger(const Event &);
+
+                void onNextFrame();
+
+                //void onAttack(const Event &);
+                //void onDamage(const Event &);
+
+                //void onGround(const Event &);
+
+                void onEnter();
+                void onLeave();
+
+                
+        };
+    }
+}
+
+#endif
+
+
